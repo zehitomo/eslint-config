b5da96ac251751cc78ecddb3b26946e592187fb0
Alexander Ogilvie <alex@zehitomo.com>
2020-12-21 13:40:30 +0900

    feature: add eslint-plugin-react rules

---
+++ react.js
@@ -0,0 +1,9 @@
+'use strict';
+
+module.exports = {
+  'react/jsx-boolean-value': ['error', 'always', { never: ['jsx'] }],
+  'react/jsx-uses-react': 'off',
+  'react/prop-types': 'off',
+  'react/react-in-jsx-scope': 'off',
+  'react/self-closing-comp': 'error',
+};
