45787e659879dd0167a7d8369f4bbb4d1bdd4663
Alexander Ogilvie <alex@zehitomo.com>
2020-12-22 11:13:34 +0900

    chore: add existing eslint rules to react config

--- react.js
+++ react.js
@@ -2,8 +2,15 @@
 
 module.exports = {
   'react/jsx-boolean-value': ['error', 'always', { never: ['global', 'jsx'] }],
+  'react/jsx-no-bind': 'error',
+  'react/jsx-pascal-case': 'error',
   'react/jsx-uses-react': 'off',
+  'react/no-array-index-key': 'error',
+  'react/no-string-refs': 'error',
   'react/no-unused-prop-types': 'error',
+  'react/prefer-es6-class': 'error',
+  'react/prefer-stateless-function': 'error',
   'react/react-in-jsx-scope': 'off',
+  'react/require-render-return': 'error',
   'react/static-property-placement': ['error', 'static public field'],
 };
