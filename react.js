8d5eb417f4cb8623f90bf0e8557158be0be50846
Alexander Ogilvie <alex@zehitomo.com>
2020-12-22 11:07:51 +0900

    doc: react/destructuring-assignment

--- react.js
+++ react.js
@@ -1,6 +1,7 @@
 'use strict';
 
 module.exports = {
+  'react/destructuring-assignment': 'error',
   'react/jsx-boolean-value': ['error', 'always', { never: ['global', 'jsx'] }],
   'react/jsx-uses-react': 'off',
   'react/no-unused-prop-types': 'error',
