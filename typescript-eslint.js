98f7623ad0b5ab51551741ad927cad2c02b44532
dricholm <david.richolm@gmail.com>
2021-01-26 11:39:32 +0100

    Use TypeScript's no-shadow in TS files

--- typescript-eslint.js
+++ typescript-eslint.js
@@ -5,6 +5,7 @@ module.exports = {
     '@typescript-eslint/array-type': ['error', { default: 'generic' }],
     '@typescript-eslint/ban-ts-comment': ['error', { 'ts-expect-error': 'allow-with-description' }],
     '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-shadow': 'warn',
     '@typescript-eslint/no-unnecessary-boolean-literal-compare': 'error',
     '@typescript-eslint/no-unnecessary-condition': 'error',
     '@typescript-eslint/no-unused-vars': 'error',
@@ -15,5 +16,6 @@ module.exports = {
     '@typescript-eslint/prefer-string-starts-ends-with': 'error',
     '@typescript-eslint/prefer-ts-expect-error': 'error',
     '@typescript-eslint/restrict-plus-operands': 'error',
+    'no-shadow': 'off', // replaced by TypeScript no-shadow
   },
 };
